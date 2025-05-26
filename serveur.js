442af2399f08c2616750e0c3cbfb69e9290f222e
Arthur C <arthurcorroyez.arthurandco@gmail.com>
2025-05-26 21:02:54 +0200

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -5,14 +5,12 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Connexion
 app.post('/connexion', (req, res) => {
   const { identifiant, motdepasse } = req.body;
 
@@ -28,14 +26,12 @@ app.post('/connexion', (req, res) => {
 
     if (utilisateur && utilisateur.motdepasse === motdepasse) {
       res.status(200).json({ message: "Connexion réussie !" });
-      
     } else {
       res.status(401).json({ message: "Identifiant ou mot de passe incorrect." });
     }
   });
 });
 
-// Démarrage
 app.listen(PORT, () => {
   console.log(`Serveur démarré : http://localhost:${PORT}`);
 });
