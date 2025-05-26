709219cac0ba26f06a9edb78d2609e62a4a84a68
Arthur C <arthurcorroyez.arthurandco@gmail.com>
2025-05-26 21:13:10 +0200

    Update login.js

--- public/login.js
+++ public/login.js
@@ -2,7 +2,7 @@ document.querySelector("button").addEventListener("click", () => {
   const identifiant = document.getElementById("identifiant").value;
   const motdepasse = document.getElementById("motdepasse").value;
 
-  fetch("http://localhost:3000/connexion", {
+  fetch("/connexion", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ identifiant, motdepasse }),
